70dbc77c9e0067c88ccf4f0b8a9cddba3eb6e732
Dao Huy Vu <huyvu160102@gmail.com>
2024-02-25 18:01:59 +0700

    Change Buff size

--- scull/test-scull.c
+++ scull/test-scull.c
@@ -5,14 +5,14 @@
 int main(void) {
 
     int scull;
-    char buf[12800];
-
+    char buf[128];
+	
     scull = open("/dev/scull", O_RDONLY);
     if (scull < 0)
         return 1;
 
     lseek(scull, 3999, SEEK_SET);
-    read(scull, buf, 12800);
+    read(scull, buf, 128);
 
     close(scull);
     return 0;
